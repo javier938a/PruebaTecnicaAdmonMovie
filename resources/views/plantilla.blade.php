9d71d3c2a64fad391fa4c32b21f3aa4db8e0dd7e
Javier <carlos8a_93@yahoo.com>
2021-03-02 22:51:13 -0600

    eliminando un enlace

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -27,7 +27,7 @@
     <!--Aqui la barra de navegacion-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Inicio</a>
+        <a class="navbar-brand" href="{{ route('index') }}">Inicio</a>
         <button
         class="navbar-toggler"
         type="button"
@@ -41,9 +41,6 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Inicio</a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('movie') }}">Peliculas</a>
             </li>
